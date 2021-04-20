042a1813d92c411c92494a5bce10c118746b40c9
ZhangPeng <36990035+KrissZhang@users.noreply.github.com>
2021-04-20 15:45:25 +0800

    init

---
+++ cloud-server-two-interface/src/test/java/com/self/cloudserver/CloudserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.self.cloudserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
