71af95ad2ff694c0c2ac8ec332ebc1890dd67a98
ex_zhangpeng3@exiot.cmcc <Zp!18716331455>
2022-03-16 16:01:50 +0800

    bean处理

--- cloud-server-two-server/src/main/java/com/self/cloudserver/utils/BeanUtil.java
+++ cloud-server-two-server/src/main/java/com/self/cloudserver/utils/BeanUtil.java
@@ -1,12 +1,16 @@
 package com.self.cloudserver.utils;
 
+import com.google.common.base.CaseFormat;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.BeanWrapper;
 import org.springframework.beans.BeanWrapperImpl;
+import org.springframework.util.CollectionUtils;
 
 import java.beans.PropertyDescriptor;
+import java.lang.reflect.Field;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class BeanUtil {
 
@@ -30,6 +34,54 @@ public class BeanUtil {
 
         String[] notNullField = new String[notNullFieldSet.size()];
         return notNullFieldSet.toArray(notNullField);
+    }
+
+    public static <T> T copyProperties(Object source, Class<T> tClass) {
+        try {
+            if (source == null) {
+                return null;
+            }
+
+            T newInstance = tClass.newInstance();
+            org.springframework.beans.BeanUtils.copyProperties(source, newInstance);
+
+            return newInstance;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public static Map<String, Object> beanToMapNotNull(Object o){
+        Map<String, Object> result = new HashMap<>();
+        Class<?> tClass = o.getClass();
+        Field[] fields = tClass.getDeclaredFields();
+        for(Field f : fields){
+            f.setAccessible(true);
+            try {
+                String name = f.getName();
+                String key = CaseFormat.LOWER_CAMEL.to(CaseFormat.LOWER_UNDERSCORE, name);
+                Object o1 = f.get(o);
+                if(o1 == null){
+                    continue;
+                }else if(o1 instanceof String && StringUtils.isEmpty((String)o1)){
+                    continue;
+                }else{
+                    result.put(key, o1);
+                }
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return result;
+    }
+
+    public static <E, T> List<E> copyList(List<T> srcList, Class<E> clazz) {
+        if (CollectionUtils.isEmpty(srcList)) {
+            return Collections.emptyList();
+        }
+
+        return srcList.stream().map(src -> copyProperties(src, clazz)).collect(Collectors.toList());
     }
 
 }
