c14eb8deb712f332cbae66b8d993c2aef85e9b76
ex_zhangpeng3@exiot.cmcc <Zp!18716331455>
2022-04-18 09:41:15 +0800

    OKHttp

--- cloud-server-two-server/src/test/java/com/self/cloudserver/CloudserverApplicationTests.java
+++ cloud-server-two-server/src/test/java/com/self/cloudserver/CloudserverApplicationTests.java
@@ -1,9 +1,13 @@
 package com.self.cloudserver;
 
+import com.google.common.collect.Maps;
 import com.self.cloudserver.thread.AsyncManager;
+import com.self.cloudserver.utils.http.BaseHttpRequest;
+import com.self.cloudserver.utils.http.OkHttpUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Map;
 import java.util.TimerTask;
 
 @SpringBootTest
@@ -33,6 +37,23 @@ class CloudserverApplicationTests {
         long endMs = System.currentTimeMillis();
 
         System.out.println("消耗了" + (endMs - startMs) + "毫秒");
+    }
+
+    @Test
+    public void testOkHttp(){
+        String url = "https://wis.qq.com/weather/common";
+        Map<String, Object> reqMap = Maps.newHashMap();
+        reqMap.put("source", "pc");
+        reqMap.put("weather_type", "forecast_24h");
+        reqMap.put("province", "重庆市");
+        reqMap.put("city", "重庆市");
+        reqMap.put("county", "渝北区");
+
+        BaseHttpRequest request = new BaseHttpRequest(url, BaseHttpRequest.Method.GET);
+        request.getQueryParams().putAll(reqMap);
+        String response = OkHttpUtil.getInstance().sendRequest(request);
+
+        System.out.println(response);
     }
 
 }
