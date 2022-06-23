be5693c47f65d604a237d0199ab7c9b684090332
ex_zhangpeng3@exiot.cmcc <Zp!18716331455>
2022-06-23 15:40:33 +0800

    Kafka

--- cloud-server-two-server/src/main/java/com/self/cloudserver/kafka/listener/RecordKafkaReceiver.java
+++ cloud-server-two-server/src/main/java/com/self/cloudserver/kafka/listener/RecordKafkaReceiver.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.self.cloudserver.dto.KafkaCommonReq;
 import com.self.cloudserver.kafka.constants.TopicConstant;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,10 +21,11 @@ public class RecordKafkaReceiver {
     private static Logger logger = LoggerFactory.getLogger(RecordKafkaReceiver.class);
 
     @KafkaListener(topics = {TopicConstant.TEST_RECORD})
-    public void listen(String rel, Acknowledgment ack) throws JsonProcessingException {
-        KafkaCommonReq req = objectMapper.readValue(rel, KafkaCommonReq.class);
-        System.out.println(req.toString());
+    public void listen(ConsumerRecord<String, String> consumerRecord, Acknowledgment ack) throws JsonProcessingException {
         ack.acknowledge();
+
+        KafkaCommonReq req = objectMapper.readValue(consumerRecord.value(), KafkaCommonReq.class);
+        System.out.println(req.toString());
     }
 
 }
